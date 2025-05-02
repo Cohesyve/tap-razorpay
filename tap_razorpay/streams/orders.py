7e1dc49c14c4a26a0ab109c4d90d1d8a91d03942
Anup <anup.tiwari@cohesyve.com>
2025-05-02 16:57:49 +0530

    updated pagination

--- tap_razorpay/streams/orders.py
+++ tap_razorpay/streams/orders.py
@@ -1,10 +1,8 @@
 from tap_razorpay.streams.base import PaginatedStream
-
 import singer
 import json
 
 LOGGER = singer.get_logger()  # noqa
-
 
 class OrdersStream(PaginatedStream):
     API_METHOD = 'GET'
@@ -15,7 +13,22 @@ class OrdersStream(PaginatedStream):
     def api_path(self):
         return '/orders'
 
+    def get_url(self, next_page_token=None):
+        skip_value = next_page_token or 0  
+        url = f"{self.api_path}?skip={skip_value}?count=100"
+        return url
+
+    def get_next_page_token(self, response):
+      
+        if 'items' in response and len(response['items']) > 0:
+           
+            return response['skip'] + 1  
+        return None
+
     def get_stream_data(self, result):
+        """
+        Extract and transform the relevant records from the paginated response.
+        """
         return [
             self.transform_record(record)
             for record in result['items']
